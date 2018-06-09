49bdb551d90e76d4abff806a85424adb85e2c6ca
gozhack <secto8@hotmail.com>
2018-06-09 10:32:32 -0500

    Commit desde VS2017, primero proyecto OFZ

---
+++ OFZ/StandarXamarin/StandarXamarin/StandarXamarin/MyPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace StandarXamarin
+{
+    public class MyPage : ContentPage
+    {
+        public MyPage() {
+            var produtos = new[] {
+                "Producto1",
+                "Producto2",
+                "Producto3",
+                "Producto4",
+                "Producto5",
+                "Producto6"
+            };
+
+            var listaProductos = new ListView();
+            listaProductos.ItemsSource = produtos;
+
+            Content = listaProductos;
+
+
+
+
+
+        }
+
+    }
+}
