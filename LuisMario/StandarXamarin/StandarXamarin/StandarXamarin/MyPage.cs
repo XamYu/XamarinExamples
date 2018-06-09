069338bf73494f44f4c7d3baf9344b9e3a931bd7
edesarrollo.ti@grupoyuyin.com.mx <edesarrollo.ti@grupoyuyin.com.mx>
2018-06-09 10:33:03 -0500

    Commit desde VS 2017, primer proyecto LMEP

---
+++ LuisMario/StandarXamarin/StandarXamarin/StandarXamarin/MyPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace StandarXamarin
+{
+    public class MyPage : ContentPage
+    {
+
+        public MyPage()
+        {
+
+            var productos = new[] {
+                "Producto1",
+                "Producto2",
+                "Producto3",
+                "Producto4",
+                "Producto5",
+                "Producto6"
+            };
+
+            var listaProductos = new ListView();
+            listaProductos.ItemsSource = productos;
+
+
+            Content = listaProductos;
+
+        }
+
+    }
+}
