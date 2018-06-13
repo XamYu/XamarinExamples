9532c6f6ef6b4350604a983c534d9c933fbb18d3
jrazo92 <jrazo92@hotmail.com>
2018-06-12 20:09:33 -0500

    gitignore are with exclusion folders, test from another account

---
+++ Razo/AppRaz/AppRaz/AppRaz/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AppRaz.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
