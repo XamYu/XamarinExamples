71fafe9b6839b7df2cafd0ecd2722fd7a7d4d9b8
FTECNOLOGIA <F.TECNOLOGIA@GRUPOYUYIN.COM.MX>
2018-06-14 10:45:25 -0500

    primer commit DBSH
    Proyecto Standar

---
+++ DBSH/StandarXamarin/StandarXamarin/StandarXamarin/MyPage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace StandarXamarin
+{
+   public class MyPage : ContentPage
+    {
+        public MyPage() {
+            var vProductos = new[] {
+                "Producto1",
+                "Producto2",
+                "Producto3",
+                "Producto4",
+                "Producto5",
+                "Producto6",
+            };
+
+            Label Nombre = new Label
+            {
+                Text = "Daniela",
+                FontSize = 20,
+                FontAttributes = FontAttributes.Bold,
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            var entryNombre = new Entry
+            {
+                Placeholder = "Escribe aqui tu nombre",
+                Text = "Daniela Sánchez"
+            };
+
+            var actualizarNombre = new Button
+            {
+                Text = "Actualiza nombre",
+                Font = Font.SystemFontOfSize(NamedSize.Large),
+                BorderWidth = 1,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+
+            };
+
+            var vListaProductos = new ListView();
+            vListaProductos.ItemsSource = vProductos;
+
+            Content = new StackLayout
+            {
+                Children = {
+                    Nombre,
+                    entryNombre,
+                    actualizarNombre,
+                    vListaProductos
+                }
+            };
+        }
+    }
+}
